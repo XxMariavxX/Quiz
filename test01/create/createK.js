4e1e03864cefb0d2ee5f4dd90dad74aa5b5d76dc
Kolya <kolagusak752@gmail.com>
2025-11-09 22:22:38 +0200

    small changes

---
+++ test01/create/createK.js
@@ -0,0 +1,5 @@
+const title = document.getElementById('title')
+const description = document.getElementById('description')
+const add_question = document.getElementById('add_question')
+const add_answer = getElementById('add_answer')
+
