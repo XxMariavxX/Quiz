b1281931a9a6479e86dcde06adc15f0b620ccbfa
XxMariavxX <synevicmaria13@gmail.com>
2025-11-10 19:26:28 +0200

    create(addQuestion and addAnswer)

--- src/create/create.js
+++ src/create/create.js
@@ -13,73 +13,28 @@ createQuiz.addEventListener("click", function () {
     "beforeend",
     `<br>
         <fieldset>
-        <button type="button" id="add-question">Add question</button>
+        <button type="button" id="addquestion">Add question</button>
         <button type="button" id="add-answer">Add answer</button>
         <fieldset class = "quiz" id="quiz">
-        <input type="text" id="question" placeholder= Write your question ?">
-        <input type="text" id="answer" placeholder= your answer ?">
-        <input type="text" id="answer" placeholder= your answer ?">  
+        <br>
+        <input type="text" id="question" placeholder= "Write your question ">
+        <br>
+        <input type="text" id="answer" placeholder= "your answer ">
+        <br>
+        <input type="text" id="answer" placeholder= "your answer ">  
         </fieldset>`
   );
-});
-
-
-addQuestion.addEventListener("click", function () {
-  const inputQuestion = document.createElement("div");
-  inputQuestion.insertAdjacentHTML("beforeend", function () {
+  const addQuestion = block.querySelector("#addquestion");
+  addQuestion.addEventListener("click", function () {
+    const inputQuestion = document.createElement("div");
+    inputQuestion.insertAdjacentHTML("beforeend",  
     `<br>
-    <input type="text" id="question" placeholder= Write your question ?">
-    <input type="text" id="add-question" placeholder= your answer ?">
-    <input type="text" id="add-question" placeholder= your answer ?">
-    </fieldset>
-    `;
+    <input type="text" id="question" placeholder=" Write your question ?">
+    <br>
+    <input type="text" id="add-answer" placeholder= "your answer">
+    <br>
+    <input type="text" id="add-answer" placeholder= "your answer">
+    `);
+    field__1.appendChild(inputQuestion);
   });
-  field__1.appendChild(inputQuestion);
 });
-
-
-// addAnswer.addEventListener("click", function () {
-//   const inputAnswer = document.createElement("input");
-//   field__1.appendChild(inputAnswer);
-// });
-
-// createQuiz.addEventListener("click", function () {
-//     if (!block || !createQuiz) return;
-//     block.insertAdjacentHTML("beforeend",
-//         `<br>
-//         <button type="button" id="add-question">Add question</button>
-//         <button type="button" id="add-answer">Add answer</button>
-//         <fieldset class = "question" >
-//         </fieldset>
-//         `)
-// });
-
-// addQuestion.addEventListener("click", function () {
-//     field__1.insertAdjacentHTML(".question",`
-//         <fieldset>
-//         <input type="text" id="add-question" placeholder="Write your question ?">
-//         </fieldset>
-//         <fieldset>
-//         <input type="text" id="add-question" placeholder= your answer ?">
-//         <input type="text" id="add-question" placeholder= your answer ?">
-//         </fieldset>
-//         `)
-// });
-
-// addAnswer.addEventListener("click", function () {
-//     field__1.insertAdjacentHTML("beforeend",`
-//         <fieldset>
-//         <input type="text" id="add-question" placeholder= your answer">
-//         </fieldset>
-//         `)
-// });
-
-// addQuestion.addEventListener("click", function () {
-//     const inputQuestion = document.createElement("input")
-//     field.appendChild(inputQuestion)
-// });
-
-// addAnswer.addEventListener("click", function(){
-//     const inputAnswer =document.createElement("input")
-//     field.appendChild(inputAnswer)
-// })
--- src/create/createM.js
+++ src/create/createM.js
@@ -33,3 +33,51 @@ addAnswer.addEventListener("click", function () {
   const inputAnswer = document.createElement("input");
   field__1.appendChild(inputAnswer);
 });
+// quizElement.querySelector("#delete").addEventListener("click", e => {
+//     e.target.closest(".question").remove();
+// });
+// addAnswer.addEventListener("click", function () {
+//   const inputAnswer = document.createElement("input");
+//   field__1.appendChild(inputAnswer);
+// });
+
+// createQuiz.addEventListener("click", function () {
+//     if (!block || !createQuiz) return;
+//     block.insertAdjacentHTML("beforeend",
+//         `<br>
+//         <button type="button" id="add-question">Add question</button>
+//         <button type="button" id="add-answer">Add answer</button>
+//         <fieldset class = "question" >
+//         </fieldset>
+//         `)
+// });
+
+// addQuestion.addEventListener("click", function () {
+//     field__1.insertAdjacentHTML(".question",`
+//         <fieldset>
+//         <input type="text" id="add-question" placeholder="Write your question ?">
+//         </fieldset>
+//         <fieldset>
+//         <input type="text" id="add-question" placeholder= your answer ?">
+//         <input type="text" id="add-question" placeholder= your answer ?">
+//         </fieldset>
+//         `)
+// });
+
+// addAnswer.addEventListener("click", function () {
+//     field__1.insertAdjacentHTML("beforeend",`
+//         <fieldset>
+//         <input type="text" id="add-question" placeholder= your answer">
+//         </fieldset>
+//         `)
+// });
+
+// addQuestion.addEventListener("click", function () {
+//     const inputQuestion = document.createElement("input")
+//     field.appendChild(inputQuestion)
+// });
+
+// addAnswer.addEventListener("click", function(){
+//     const inputAnswer =document.createElement("input")
+//     field.appendChild(inputAnswer)
+// })
