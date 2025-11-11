3e1e71ccd4ebd285bcf2bf7c8f8bd0b36b05c10d
XxMariavxX <synevicmaria13@gmail.com>
2025-11-11 12:43:31 +0200

    home.js

--- src/create/create.js
+++ src/create/create.js
@@ -1,7 +1,6 @@
 const title = document.getElementById("title");
 const description = document.getElementById("description");
 const block = document.getElementById("field");
-const block1 = document.getElementById("field__1");
 const create = document.getElementById("create");
 const createQuiz = document.getElementById("createQuiz");
 const container = document.getElementById("create-quiz");
@@ -12,7 +11,6 @@ createQuiz.addEventListener("click", function () {
   block.insertAdjacentHTML(
     "beforeend",
     `<br>
-        <fieldset>
         <button type="button" id="addquestion">Add question</button>
         <button type="button" id="add-answer">Add answer</button>
         <fieldset class = "quiz" id="quiz">
@@ -21,22 +19,31 @@ createQuiz.addEventListener("click", function () {
         <br>
         <input type="text" id="answer" placeholder= "your answer ">
         <br>
-        <input type="text" id="answer" placeholder= "your answer ">  
-        </fieldset>`
+        <input type="text" id="answer" placeholder= "your answer ">  `
   );
-  const addQuestion = block1.querySelector("#addquestion");
+  const addQuestion = block.querySelector("#addquestion");
   addQuestion.addEventListener("click", function () {
     const inputQuestion = document.createElement("div");
-    inputQuestion.insertAdjacentHTML("beforeend",  
-    `<br>
+    inputQuestion.insertAdjacentHTML(
+      "beforeend",
+      `<br>
     <input type="text" id="question" placeholder=" Write your question ?">
     <br>
     <input type="text" id="add-answer" placeholder= "your answer">
     <br>
     <input type="text" id="add-answer" placeholder= "your answer">
-    `);
-    field__1.appendChild(inputQuestion);
+    `
+    );
+    block.appendChild(inputQuestion);
   });
-  // const addAnswer = block1.querySelector("#answer");
-  // addAnswer.
+  const addAnswer = block.querySelector("#answer");
+  addAnswer.addEventListener("click", function () {
+    const inputAnswer = document.createElement("div");
+    inputAnswer.insertAdjacentHTML(
+      "beforeend",
+    `<br>
+    <input type="text" id="add-answer" placeholder= "your answer">`
+    );
+    block.appendChild(inputAnswer);
+  });
 });
