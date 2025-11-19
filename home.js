c4818ed255667fb853f11496df5d30a793bdcb2c
Kolya <kolagusak752@gmail.com>
2025-11-19 23:19:55 +0200

    card

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-function escapeHtml(unsafe) {
+function escapeHTML(unsafe) {
   return unsafe
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
@@ -7,10 +7,43 @@ function escapeHtml(unsafe) {
     .replace(/'/g, "&#039;");
 }
 
+const quizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+quizzes.forEach(renderquiz);
+console.log(localStorage.getItem("quizzes"));
 
-const section =document.querySelector("quiz-card");
+function renderquiz(quiz, index) {
+  const section = document.getElementById("quiz-card");
+  const card = document.createElement("div");
+  card.classList.add("quiz");
 
-function quizCard (){
-    section.innerHtml()
+  card.innerHTML = `
+    <h2>${escapeHTML(quiz.title)}</h2>
+    <p>${escapeHTML(quiz.description)}</p>
+    <button class="delete">❌</button>
+    <button class="start">Start</button>
+  `;
+
+  section.appendChild(card);
+
+  const deleteButton = card.querySelector(".delete");
+  deleteButton.addEventListener("click", () => {
+    if (confirm("Ви точно хочете видалити квіз?")) {
+      card.remove();               
+      quizzes.splice(index, 1);    
+      localStorage.setItem("quizzes", JSON.stringify(quizzes));
+    }
+  });
+
+  const startButton = card.querySelector(".start");
+  startButton.addEventListener("click", () => {
+    console.log("Назва квізу:", quiz.title);
+    
+  });
 }
+
+
+
+
+
+
     
