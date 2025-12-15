acd3b46ad62fdac561fe0ad832b18fb7957ea943
XxMariavxX <synevicmaria13@gmail.com>
2025-12-15 22:10:35 +0200

    result bunny

--- src/result/result.js
+++ src/result/result.js
@@ -8,7 +8,7 @@ result = result[result.length - 1];
 const questions = result.questions;
 let score = 0;
 
-let maxScore =quizzes.amount;
+let maxScore = quizzes.amount;
 
 for (let i = 0; i <= questions.length - 1; i++) {
   const correct_answersIndex = [];
@@ -17,12 +17,15 @@ for (let i = 0; i <= questions.length - 1; i++) {
   const correct_array = questions[i].correct_answers;
   let correct = 0;
   let wrong = 0;
+
   for (let j = 0; j <= correct_array.length - 1; j++) {
     correct_answersIndex.push(correct_array[j].answerIndex);
   }
+
   for (let k = 0; k <= user_array.length - 1; k++) {
     user_answersIndex.push(user_array[k].answerIndex);
   }
+
   for (let answer of user_answersIndex) {
     if (correct_answersIndex.includes(answer)) {
       correct += 1;
@@ -30,7 +33,9 @@ for (let i = 0; i <= questions.length - 1; i++) {
       wrong += 1;
     }
   }
+
   let score_question = (correct - wrong) / correct_answersIndex.length;
+
   if (Number.isNaN(score_question)) {
     score_question = 0;
   } else {
@@ -38,9 +43,34 @@ for (let i = 0; i <= questions.length - 1; i++) {
   }
 }
 
+const userScore = Number(score.toFixed(2));
+
+let img = "";
+
+if (userScore === maxScore) {
+  img = "<img class = 'reaction' src='../../public/rabbit.gif'>"
+
+} else if (maxScore > userScore && userScore >= maxScore / 2) {
+  img = "<img class = 'reaction' src='../../public/rabbit.gif'>"
+} else {
+  img = "<img class = 'reaction' src='../../public/rabbit.gif'>"
+}
+
+section.insertAdjacentHTML(
+  'beforeend',
+  img
+)
+
 section.insertAdjacentHTML(
   "afterbegin",
   `<div class = "score">
-    Your score: ${Number(score.toFixed(2))}/${maxScore}
+    Your score ♥ : ${userScore}/${maxScore}
     </div>`
 );
+
+section.insertAdjacentHTML(
+  'afterend',
+  `
+  <a href="../quiz/quiz.html"><button type="button" class = "tryagain">Try again</button></a>
+  `
+)
